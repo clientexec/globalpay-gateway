33d2de77a9c2a35086d8af15f57ba22c577afe3c
Matt Grandy <matt@clientexec.com>
2014-04-01 00:12:11 -0400

    Change Check CVV to be hidden, and always on

--- PluginGlobalpay.php
+++ PluginGlobalpay.php
@@ -93,7 +93,7 @@ class PluginGlobalpay extends GatewayPlugin
                                         "value"         =>"0"
                                        ),
                    /*T*/"Check CVV2"/*/T*/ => array (
-                                        "type"          =>"yesno",
+                                        "type"          =>"hidden",
                                         "description"   =>/*T*/"Select YES if you want to accept CVV2 for this plugin."/*/T*/,
                                         "value"         =>"1"
                                        )
